746d59dac9f2b7f95f7c7eff6876fc7010f5c3a3
mto99 <ozmen.muhammed@outlook.com>
2022-11-03 18:45:34 +0100

    added start method

--- source/app.py
+++ source/app.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import os
 
 app = Flask(__name__)
+
+logged_in=False
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+@app.route("/camera")
+def camera():
+    return Response()
+
+
 @app.route("/login")
 def login():
     # verify user
     return render_template("login.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
