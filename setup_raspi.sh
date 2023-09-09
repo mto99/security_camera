2acea23be6f8cfe19cdbb1a4523059a24c4f0ef7
mto99 <ozmen.muhammed@outlook.com>
2023-09-09 19:21:14 +0200

    added raspi setup bash script

---
+++ setup_raspi.sh
@@ -0,0 +1,42 @@
+echo "=============================\n SETUP PROCESS STARTS..."
+sudo apt install python3-pip -y
+sudo apt install git -y
+
+
+echo "------------------------------CAMERA------------------------------"
+
+
+sudo rpi-update -y
+
+echo /opt/vc/lib/ | sudo tee /etc/ld.so.conf.d/vc.conf
+sudo ldconfig
+
+
+curl https://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
+echo "deb https://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" | sudo tee /etc/apt/sources.list.d/uv4l.list
+
+sudo apt-get update
+sudo apt-get install uv4l uv4l-raspicam -y
+
+sudo reboot #to activate software
+
+# Enable driver support for rasperry pi zero
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install libedgetpu1-max -y
+
+sudo apt-get install -y uv4l-raspicam-ai uv4l-raspicam-extras
+
+sudo service uv4l_raspicam restart
+
+sudo apt-get install -y uv4l-tc358743-extras
+
+sudo raspi-config
+
+sudo rpi-update
+
+uv4l --driver raspicam --auto-video_nr --width 640 --height 480 --encoding jpeg
+dd if=/dev/video0 of=snapshot.jpeg bs=11M count=1
+
+
